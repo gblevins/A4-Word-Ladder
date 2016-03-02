8701610bdfa6202e3fc115feb25c18b410e40b7f
Garret Blevins <gblevins7@utexas.edu>
2016-03-02 14:15:12 -0600

    Added JUnit Test File

---
+++ src/assignment4/WordLadderSolverTest.java
@@ -0,0 +1,15 @@
+package assignment4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordLadderSolverTest {
+
+	@Test
+	public void testComputeLadder()
+	{
+		fail("Not yet implemented");
+	}
+
+}
