140c0d766af11c4ab4fcd362566118301b973f68
Garret Blevins <gblevins7@utexas.edu>
2016-03-02 16:31:17 -0600

    test stuff

--- src/assignment4/WordLadderSolverTest.java
+++ src/assignment4/WordLadderSolverTest.java
@@ -2,14 +2,44 @@ package assignment4;
 
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Test;
 
 public class WordLadderSolverTest {
-
+	// main is expecting 2 arguments
+	// dictionary file name first (args[0])
+    public void main(String[] args)
+    {
+    	if (args.length != 1)
+		{
+			System.err.println ("Error: Incorrect number of command line arguments");
+			System.exit(-1);
+		}
+		wordLadderSolver = new WordLadderSolver(args[0]);
+		testComputeLadder();
+    }
+    
+	Assignment4Interface wordLadderSolver;
+    
 	@Test
 	public void testComputeLadder()
 	{
-		fail("Not yet implemented");
+		try
+		{
+			List<String> result = wordLadderSolver.computeLadder("money", "honey");
+			Iterator<String> a = result.iterator();
+			while(a.hasNext())
+			{
+				System.out.println(a.next());
+			}
+			System.out.println("**********");
+		} 
+		catch (NoSuchLadderException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
 	}
-
 }
